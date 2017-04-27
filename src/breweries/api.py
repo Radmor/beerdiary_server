3d0e8d9a46507ea02336050e3e66e2cbff824bad
Piotr Hankiewicz <piotr.hankiewicz@student.put.poznan.pl>
2017-04-27 20:15:56 +0200

    pagination for breweries removed

--- src/breweries/api.py
+++ src/breweries/api.py
@@ -1,14 +1,9 @@
 from rest_framework import viewsets, permissions
-from rest_framework import pagination
 
 from .models import Brewery
 from .serializers import BrewerySerializer
-
-class CustomPagination(pagination.PageNumberPagination):
-    page_size = 2
 
 class BreweryViewSet(viewsets.ModelViewSet):
     serializer_class = BrewerySerializer
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Brewery.objects.all()
-    pagination_class = CustomPagination
