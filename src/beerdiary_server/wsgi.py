c0ef29398e0934d1587c30fea54cd660e923f9e0
Piotr Hankiewicz <piotr.hankiewicz@student.put.poznan.pl>
2017-05-12 18:52:06 +0200

    wsgi test fix for ci

--- src/beerdiary_server/wsgi.py
+++ src/beerdiary_server/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "beerdiary_server.settings")
+                      "src.beerdiary_server.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
